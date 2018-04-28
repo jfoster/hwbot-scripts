1f8dced305dc7e0ecbbb340988f2c7bc0cb52d35
jfoster <jfoster@users.noreply.github.com>
2018-04-28 02:01:52 +0100

    Fix typo

--- Utils/set-maxmem.ps1
+++ Utils/set-maxmem.ps1
@@ -1,6 +1,6 @@
 $mem = ((Read-host -Prompt "Maximum Memory in MB: ")/1)*1024*1024
 if ($mem -eq 0) {
-	bcdedit /deletevalue truncatememore
+	bcdedit /deletevalue truncatememory
 } else {
 	bcdedit /set truncatememory $mem
 }
