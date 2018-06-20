a6bfa3fba12bbb8ea31a07e255f522d570079f76
jfoster <jfoster@users.noreply.github.com>
2018-06-20 17:34:27 +0100

    Fixed set-removemem.ps1 l10n issue with string formatting

--- Utils/set-removemem.ps1
+++ Utils/set-removemem.ps1
@@ -3,10 +3,10 @@ If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 	Exit
 }
 
-$meminstalled = [int](Get-WmiObject Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum | Foreach {"{0:N2}" -f ([math]::round(($_.Sum / 1MB),2))})
+$meminstalled = [int](Get-WmiObject Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum | Foreach {"{0:F0}"-f ([math]::round(($_.Sum / 1MB),2))})
 $choice = ((Read-host -Prompt "Max Memory in MB: ")/1)
 $sum = $meminstalled - $choice
-if ($choice -eq 0) {
+if ($choice -le 0) {
 	bcdedit /deletevalue removememory
 } else {
 	bcdedit /set removememory $sum
